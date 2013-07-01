4b77bc6ed47e78664ab4c828563f61a7dadccdda
Debalina Mukherjee <dmukherjee@kreeti.com>
2013-07-01 16:35:01 +0530

    Add a full_title helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
   has_many :microposts
+
+  attr_accessor :name, :email
+
+  def initialize(attributes = {})
+    @name  = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
 end
